08afe3b502afacd8cef2b79cf055796ff0741213
cocytusdedi <cocytusdedi@tutanota.com>
2024-09-22 18:40:02 +0100

    Added prototype code

--- src/main.c
+++ src/main.c
@@ -1,5 +1,39 @@
 #include <libfreedom.h>
+#include <stdio.h>
+#include <wchar.h>
+#include <locale.h>
+
+wchar_t bool_to_mark(int freedom_int) {
+    switch (freedom_int) {
+        case 0:
+            return L'✗';
+        case 1:
+            return L'✓';
+        default:
+            return L'-';
+    }
+}
 
 int main(int argc, char *argv[]) {
-    // Will call libfreedom
+    setlocale(LC_ALL, "");
+
+    SystemDataResult system_data_result = get_system_data();
+    if (system_data_result.status != 0) {
+        fprintf(stderr, "failed to get system data: %s\n", system_data_result.error);
+        return 1;
+    }
+
+    SystemFreedomResult system_freedom_result = get_system_freedom(&system_data_result.system_data);
+
+    if (system_freedom_result.status != 0) {
+        fprintf(stderr, "failed to get system freedom: %s\n", system_freedom_result.error);
+        return 1;
+    }
+
+    wprintf(L"os: %s %lc\n", system_data_result.system_data.os_name,
+            bool_to_mark(system_freedom_result.system_freedom.os));
+    wprintf(L"architecture: %s %lc\n", system_data_result.system_data.architecture,
+            bool_to_mark(system_freedom_result.system_freedom.architecture));
+    wprintf(L"firmware: %s %lc\n", system_data_result.system_data.firmware,
+            bool_to_mark(system_freedom_result.system_freedom.firmware));
 }
